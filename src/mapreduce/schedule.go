48408a8ffbfb0f4a318a594b95146c22484b6788
takunnithan <takunnithan@gmail.com>
2019-09-08 01:23:39 +0530

    Fixed Distributed Part III

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -5,6 +5,10 @@ import (
 	"sync"
 )
 
+type Response struct {
+	Message string
+}
+
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
 // or reducePhase). the mapFiles argument holds the names of the files that
@@ -15,63 +19,41 @@ import (
 // existing registered workers (if any) and new ones as they register.
 //
 
-
-
-//TODO:
-//	1) Adding workers back to the channel once they are free
-//	2) Shutdown reply has some issue
-
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
-	type Response struct {
-		Message string
-	}
 	response := new(Response)
+	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 	switch phase {
 	case mapPhase:
-		var wg sync.WaitGroup
 		ntasks = len(mapFiles)
 		n_other = nReduce
-		wg.Add(ntasks)
-
-		for i := 0; i < ntasks; i++ {
-			for rpcAddress := range registerChan {
-				fmt.Println("Worker Address:", rpcAddress)
-				go func(taskNumber int) {
-					taskArgs := DoTaskArgs{jobName, mapFiles[taskNumber], phase, taskNumber, n_other}
-					call(rpcAddress, "Worker.DoTask", taskArgs, response)
-					wg.Done()
-				}(i)
-				break
-			}
-		}
-		wg.Wait()
+		registerChan = distributeTasks(ntasks, registerChan, jobName, mapFiles, phase, n_other, response)
 		return
 	case reducePhase:
 		ntasks = nReduce
 		n_other = len(mapFiles)
-		var wg sync.WaitGroup
-		wg.Add(ntasks)
-		for i := 0; i < ntasks; i++ {
-			for rpcAddress := range registerChan {
-				go func(taskNumber int) {
-					taskArgs := DoTaskArgs{jobName, mapFiles[taskNumber], phase, taskNumber, n_other}
-					call(rpcAddress, "Worker.DoTask", taskArgs, response)
-					wg.Done()
-				}(i)
-				break
-			}
-		}
-		wg.Wait()
+		registerChan = distributeTasks(ntasks, registerChan, jobName, mapFiles, phase, n_other, response)
 		return
 	}
-	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
-
-	// All ntasks tasks have to be scheduled on workers. Once all tasks
-	// have completed successfully, schedule() should return.
-	//
-	// Your code here (Part III, Part IV).
-	//
 	fmt.Printf("Schedule: %v done\n", phase)
+}
+
+func distributeTasks(ntasks int, registerChan chan string, jobName string,
+	mapFiles []string, phase jobPhase, n_other int, response *Response) chan string {
+	var wg sync.WaitGroup
+	wg.Add(ntasks)
+	for i := 0; i < ntasks; i++ {
+		for rpcAddress := range registerChan {
+			go func(taskNumber int) {
+				taskArgs := DoTaskArgs{jobName, mapFiles[taskNumber], phase, taskNumber, n_other}
+				call(rpcAddress, "Worker.DoTask", taskArgs, response)
+				wg.Done()
+				registerChan <- rpcAddress
+			}(i)
+			break
+		}
+	}
+	wg.Wait()
+	return registerChan
 }
